f24057801808ea38741d17a862f70347bd26a391
Damilare Joseph <dre4success@live.com>
2017-06-10 23:02:12 +0100

    writing a register class for admin registration form

---
+++ www/includes/registerClass.php
@@ -0,0 +1,37 @@
+<?php
+	
+	# to insert into the admin table
+	class Register {
+
+		private $result;
+
+		public function doesEmailExist($dbconn, $email) {
+
+			$stmt = $dbconn->prepare("SELECT email FROM admin WHERE email=:em");
+			$stmt->bindParam(':em', $email);
+			$stmt->execute();
+
+			$count = $stmt->rowCount();
+			if($count > 0) {
+				$result = true;
+			}
+
+			return $result;
+
+		}
+
+		public function doAdminRegister($dbconn, $input) {
+
+			$stmt = $dbconn->prepare("INSERT INTO admin(firstname, lastname, email, hash) 
+									VALUES(:fn, :ln, :em, :hs)");
+
+			$data = [
+						':fn' => $input['fname'],
+						':ln' => $input['lname'],
+						':em' => $input['email'],
+						':hs' => $input['password']
+					];
+
+			$stmt->execute($data);
+		}
+	}
