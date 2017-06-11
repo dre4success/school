99d245f9d670e5ccdf0f16aff7fdf6a9a445f7b5
Damilare Joseph <dre4success@live.com>
2017-06-11 22:42:07 +0100

    working on admin login

---
+++ www/login.php
@@ -0,0 +1,51 @@
+<?php
+	
+		# page title
+	$title = 'School | Login';
+
+	# include db connection
+	include 'includes/db.php';
+
+	# include header
+	include 'includes/header.php';
+
+	# include functions 
+	include 'includes/functions.php';
+
+	$errors = [];
+
+	if(array_key_exists('login', $_POST)) {
+
+		if(empty($_POST['email'])) {
+			$errors['email'] = "Please Enter Your Email";
+		}
+
+		if(empty($_POST['password'])) {
+			$errors['password'] = "Please Enter Your Password";
+		}
+
+		if(empty($errors)) {
+			$clean = array_map('trim', $_POST)
+		}
+	}
+?>
+
+<div class="wrapper">
+		<h1 id="register-label">Admin Login</h1>
+				<hr>
+		<form id="register" action ="login.php" method ="POST">
+			<div>
+				<?php if(isset($errors['email'])) { echo '<span class="err">'.$errors['email'].'</span>'; } ?>
+				<label>email:</label>
+				<input type="text" name="email" placeholder="email">
+			</div>
+			<div>
+				<label>password:</label>
+				<input type="password" name="password" placeholder="password">
+			</div>
+
+			<input type="submit" name="login" value="login">
+		</form>
+
+		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
+		</div>
